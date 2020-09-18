62253211271b66fa9383a1c1e658e46e2fd6e288
EvertOjeda <evert.oj@hotmail.es>
2020-09-17 23:55:59 -0400

    commit

--- src/app/paginas/opciones/opciones.page.ts
+++ src/app/paginas/opciones/opciones.page.ts
@@ -1,4 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ThemeService } from 'src/app/servicios/theme.service';
+
+const themes={
+  autumn:{
+    primary: '#F78154',
+    secondary: '#4D9078',
+    tertiary: '#B4436C',
+    light: '#FDE8DF',
+    medium: '#FCD0A2',
+    dark: '#B89876'
+  },
+  night: {
+    primary: '#428cff',
+    secondary: '#50c8ff',
+    tertiary: '#6a64ff',
+    medium: '#989aa2',
+    dark: '#f4f5f8',
+    light: '#222428'
+  },
+  neon: {
+    primary: '#39BFBD',
+    secondary: '#4CE0B3',
+    tertiary: '#FF5E79',
+    light: '#F4EDF2',
+    medium: '#B682A5',
+    dark: '#34162A'
+
+  },
+}
+
+
 
 @Component({
   selector: 'app-opciones',
@@ -7,24 +38,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpcionesPage implements OnInit {
 
-
-  darkMode: boolean = true;
-
-  constructor() { 
-
-    const prefersDark = window.matchMedia('(prefers-color-scheme:dark)');
-    this.darkMode = prefersDark.matches;
-  }
-
-  cambio() {
-
-   // const prefersDark = window.matchMedia('(prefers-color-scheme:dark)');
-    this.darkMode = !this.darkMode;
-
-      document.body.classList.toggle('dark');
-  }
-
   ngOnInit() {
+  } 
+
+  constructor(private renderer: Renderer2, private theme:ThemeService) {}
+
+  changeTheme(name){
+    this.theme.setTheme(themes[name]);
   }
 
+
+
+  onToggleColorTheme(event) {
+    console.log(event.detail.checked);
+
+    if(event.detail.checked){
+      //document.body.setAttribute('color-theme', 'dark');
+      this.renderer.setAttribute(document.body, 'color-theme', 'dark')
+    }else{
+      //document.body.setAttribute('color-theme', 'light');
+
+      this.renderer.setAttribute(document.body, 'color-theme', 'light')
+    }
+  }
+ 
 }
--- src/app/paginas/shopping-arena/shopping-arena.page.ts
+++ src/app/paginas/shopping-arena/shopping-arena.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { EstaarenaService, estaarena } from '../../servicios/estaarena.service';
+import { NavController } from "@ionic/angular";
 
-
+import * as firebase from 'firebase';
+import { snapshotToArray } from "../../../environments/environment";
 
 
 @Component({
@@ -9,19 +11,35 @@ import { EstaarenaService, estaarena } from '../../servicios/estaarena.service';
   templateUrl: './shopping-arena.page.html',
   styleUrls: ['./shopping-arena.page.scss'],
 })
+
+
 export class ShoppingArenaPage implements OnInit {
 
+  items = [];
+  ref = firebase.database().ref('items/');
+  inputText:string='';
+  
   public estaarena : any = [];
 
-  constructor(public estaarenaservice : EstaarenaService) { }
+  constructor(public estaarenaservice : EstaarenaService, public navCtrl: NavController) { 
+    this.ref.on('value', resp=>{
+      this.items = snapshotToArray(resp);
+    });
+  }
 
-  
 
   ngOnInit() {
     this.estaarenaservice.getEstapublic().subscribe(estaarena =>{
       
       this.estaarena = estaarena;
       })
+  }
+
+  addItem(item) {
+    if(item!==undefined && item!==null){
+       let newItem = this.ref.push();
+       newItem.set(item);
+   }
   }
 
   myAction() {
